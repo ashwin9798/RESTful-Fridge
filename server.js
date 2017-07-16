8ec40a3143703e0749ad8156795514d44dd833eb
Ashwin Vivek <ashwin9798@gmail.com>
2017-07-16 12:37:31 +0800

    get and post food

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 // Get the packages we need
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var Food = require('./models/food');
+
+mongoose.connect('mongodb://localhost:27017/RESTful-Fridge');
 
 var app = express();
+
+// Use the body-parser package in our application
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Use environment defined port or 3000
 var port = process.env.PORT || 3000;
@@ -10,7 +20,47 @@ var port = process.env.PORT || 3000;
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.json({ message: 'You are running dangerously low on beer!' });
+  res.json({ message: 'You are running dangerously low on nourishment!' });
+});
+
+var foodsRoute = router.route('/foods');
+
+foodsRoute.post(function(req,res) {
+    var foodi = new Food();
+    foodi.name = req.body.name;
+    foodi.type = req.body.type;
+    foodi.quantity = req.body.quantity;
+
+    foodi.save(function(err) {
+        if(err)
+            res.send(err);
+
+        res.json({ message: 'Food added to fridge!', data: foodi });
+    });
+});
+
+//get all food in the fridge
+foodsRoute.get(function(req, res) {
+  // Use the Beer model to find all beer
+  Food.find(function(err, foods) {
+    if (err)
+      res.send(err);
+
+    res.json(foods);
+  });
+});
+
+//single item
+var foodRoute = router.route('/foods/:food_id');
+
+foodRoute.get(function(req, res) {
+  // Use the Beer model to find a specific beer
+  Food.findById(req.params.food_id, function(err, food) {
+    if (err)
+      res.send(err);
+
+    res.json(food);
+  });
 });
 
 // Register all our routes with /api
